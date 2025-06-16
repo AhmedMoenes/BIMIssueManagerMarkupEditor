455c43e742380be922548c05b5177078b012f3f1
mostafaelwahsh <mostafanabil447@gmail.com>
2025-06-16 22:55:41 +0300

    # InitViewer

--- BIMIssueManagerMarkupsEditor/Views/Viewer/ModelViewerView.xaml.cs
+++ BIMIssueManagerMarkupsEditor/Views/Viewer/ModelViewerView.xaml.cs
@@ -1,15 +1,40 @@
-﻿using System.Windows.Controls;
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Windows.Controls;
 
 namespace BIMIssueManagerMarkupsEditor.Views.Viewer
 {
-    /// <summary>
-    /// Interaction logic for ModelViewerView.xaml
-    /// </summary>
     public partial class ModelViewerView : UserControl
     {
+        private readonly ModelViewerViewModel _viewModel = new();
+
         public ModelViewerView()
         {
             InitializeComponent();
+            DataContext = _viewModel;
+            InitViewer();
         }
+
+        private async void InitViewer()
+        {
+            await WebView.EnsureCoreWebView2Async();
+
+            if (WebView.CoreWebView2 == null)
+            {
+                Console.WriteLine("❌ CoreWebView2 not initialized!");
+                return;
+            }
+
+            Console.WriteLine("✅ CoreWebView2 ready");
+
+            WebView.CoreWebView2.OpenDevToolsWindow();
+            WebView.WebMessageReceived += WebView_WebMessageReceived;
+
+            Console.WriteLine("✅ WebMessageReceived event attached");
+
+            WebView.Source = new Uri("http://localhost:5173/");
+        }
+
+       
     }
 }
