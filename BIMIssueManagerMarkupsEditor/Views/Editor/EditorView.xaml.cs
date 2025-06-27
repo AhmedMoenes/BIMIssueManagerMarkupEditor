507b2581ccdd2ab536fb041aadbae611ab0fb598
mostafaelwahsh <mostafanabil447@gmail.com>
2025-06-27 17:17:38 +0300

    #running editor without VSC

--- BIMIssueManagerMarkupsEditor/Views/Editor/EditorView.xaml.cs
+++ BIMIssueManagerMarkupsEditor/Views/Editor/EditorView.xaml.cs
@@ -31,7 +31,13 @@ namespace BIMIssueManagerMarkupsEditor.Views.Editor
         {
             await EditorWebView.EnsureCoreWebView2Async();
 
-            EditorWebView.Source = new Uri("http://localhost:5501");
+            // Get the absolute path to the local HTML file
+            string exeDir = AppDomain.CurrentDomain.BaseDirectory;
+            string localPath = System.IO.Path.Combine(exeDir, "Resources", "Editor", "index.html");
+
+            // Convert to file:// URI format and load it
+            var uri = new Uri($"file:///{localPath.Replace("\\", "/")}");
+            EditorWebView.Source = uri;
         }
 
 
